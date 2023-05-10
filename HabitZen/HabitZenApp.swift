ce518e2fec10175176091122ccc1008cbcf11580
Kacique <wbrand90@gmail.com>
2023-05-10 06:49:47 -0700

    Initial Commit

---
+++ HabitZen/HabitZenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitZenApp.swift
+//  HabitZen
+//
+//  Created by William on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitZenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
